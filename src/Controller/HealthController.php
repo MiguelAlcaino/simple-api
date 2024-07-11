fca17bb4fed6071d4a3a336c24f81de3bfe23e93
Miguel Alcaino <miguel.alcaino@ogb.cl>
2024-07-11 15:45:52 -0400

    Fix health check

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -2,13 +2,15 @@
 
 declare(strict_types=1);
 
+namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HealthController extends AbstractController
 {
-    #[Route('/health-check', name: 'health', methods: ['GET'])]
+    #[Route('/health-check', name: 'health_check', methods: ['GET'])]
     public function health(): JsonResponse
     {
         return new JsonResponse(['status' => 'ok']);
